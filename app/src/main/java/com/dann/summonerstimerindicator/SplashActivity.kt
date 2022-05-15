16a029b0cfc415ef57a5d8f005f8c7529d7e125b
DanCr <dancrkyt@gmail.com>
2022-05-15 16:31:10 -0500

    SPLASH ACTIVIRTT

---
+++ app/src/main/java/com/dann/summonerstimerindicator/SplashActivity.kt
@@ -0,0 +1,35 @@
+package com.dann.summonerstimerindicator
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
+import com.dann.summonerstimerindicator.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        // Poner transparente el status bar y la barra de acciones
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+        } else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
+        window.statusBarColor = ContextCompat.getColor(this, android.R.color.transparent)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
